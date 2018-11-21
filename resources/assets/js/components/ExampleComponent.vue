1207fd02d0ee2b8928a2158f532f475d41cfb1c2
François Guilbert <francois.guilbert@neoweb.fr>
2018-11-21 15:57:54 +0100

    test deploy

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -4,6 +4,7 @@
             <div class="col-md-8">
                 <div class="card card-default">
                     <div class="card-header">Example Component</div>
+                    
                     <div class="card-body">
                         I'm an example component.
 
