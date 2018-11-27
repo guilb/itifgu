59566eb654091f3cf00d0d0f3d0c6cd05df40376
François Guilbert <francois.guilbert@neoweb.fr>
2018-11-27 22:28:52 +0100

    order products index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,7 +23,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::paginate(config('app.pagination'));
+        $products = Product::orderBy('name', 'asc')->paginate(config('app.pagination'));
         return view('products.index', compact('products'));
     }
 
